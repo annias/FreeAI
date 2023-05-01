4a29657d801a83762595728c1245828426d86d4d
forfeited <90619153+forfeited@users.noreply.github.com>
2023-04-30 20:08:08 -0500

    new files

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-from working.emerald.main import ask
+from working.hzit.main import ask
 
-print(ask("generate a rap song about code."))
+print(ask("One topic or issue that excites a theoretical person named Likhit about the Computer Science track at tams. Don't talk about artificial intelligence talk about becoming a software architect."))
